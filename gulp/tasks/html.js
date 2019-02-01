32ac591076d9e1c50bbaaa1d267e3b2d31440eb3
kurapoko <pokopoko.mk@gmail.com>
2019-02-01 19:42:07 +0900

    設定追加

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,16 +1,16 @@
 'use strict';
 
-// プラグイン読み込み
+// プラグインの読み込み
+const fs =require('fs');
 const gulp = require('gulp');
 const ejs = require('gulp-ejs');
 const rename = require('gulp-rename');
 const plumber = require('gulp-plumber');
 
 gulp.task('ejs', function() {
-    gulp.src(
-        ['src/html/**/*.ejs','!' + 'src/html/**/_*.ejs']
-    )
-    .pipe(ejs())
-    .pipe(rename({extname: '.html'})) 
-    .pipe(gulp.dest("build/html/"))
+	var json = JSON.parse(fs.readFileSync('./src/json/pages.json')); 
+    gulp.src(['./src/html/**/*.ejs', '!./src/html/**/_*.ejs'])
+        .pipe(ejs({json:json}))
+		.pipe(rename({extname: '.html'}))
+        .pipe(gulp.dest('./build/html/'));
 });
